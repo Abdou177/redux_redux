0db80fb0b3f22493ef154f9cdb1ce07c5954717a
abdou <touisabdelbasset.com>
2021-06-01 14:08:22 +0200

    first commit

--- src/JS/reducer/TaskReducer.js
+++ src/JS/reducer/TaskReducer.js
@@ -12,9 +12,9 @@ const initialState={
         case ADD : 
         return {...state, list:[...state.list,action.payload]}
         case COMPLETE :
-            return {...state.list,list:state.list.map(el=>el.id==action.payload ? {...el,complete:!el.complete}:el)}
+            return {...state.list,list:state.list.map(el=>el.id===action.payload ? {...el,complete:!el.complete}:el)}
             case UPDATE :
-                return {...state.list,list:state.list.map(el=>el.id==action.payload.id ? {...el,input:action.payload.input,complete:false,editable:!el.editable}:el)}
+                return {...state.list,list:state.list.map(el=>el.id===action.payload.id ? {...el,input:action.payload.input,complete:false,editable:!el.editable}:el)}
             case EDITABLE:
                  return {...state.list,list: state.list.map((el) =>el.id === action.payload ? { ...el, editable: !el.editable }: el)};
             case DELETE:
